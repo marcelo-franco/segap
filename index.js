15ee6df6df2d0ec120dd9e8bca2073fef8a409aa
Marcelo Franco <marcelo.franco.br@gmail.com>
2018-08-22 11:10:56 -0300

    resolvendo um comportamento indesejado

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let commentWindow;
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.on('closed', () => app.quit());
+
     const mainMenu = Menu.buildFromTemplate(menuTemplate);
     Menu.setApplicationMenu(mainMenu);
 })
